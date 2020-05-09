82e1b53a68adcfd2abae47b894923774d8958e13
michaelsorenson <michaelsorenson427@gmail.com>
2020-05-09 16:37:55 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Description
+This repo contains a notebook to simulate a cell's ribosomes and RNA using a [simulation](https://github.com/shahpr/SMoPT) created by the [Shah lab](https://www.theshahlab.org/). The purpose of these simulations is to understand what harringtonine and cycloheximide rates correspond to what initiation & elongation rates. By understanding this relationship, we can then specify an exact initiation rate and elongation rate, and then see what those rates look like in terms of RNA ribosomal distribution. Then, we can compare these results to the Zid lab's results for cells under different types of stress (especially glucose starvation)
+
+# Installing
+
+Run these commands:
+
+```
+git clone https://github.com/michaelsorenson/zid_simulation.git
+cd zid_simulation
+git clone https://github.com/shahpr/SMoPT.git
+```
+
+Run notebook to see simulation results.
---
+++ src/run_sim.py
@@ -0,0 +1,70 @@
+"""
+run_sim.py
+Runs simulation from Shah lab: https://github.com/shahpr/SMoPT
+"""
+
+import os
+import shlex
+import shutil
+import subprocess as sp
+import time
+
+PATH_TO_SHAH = '../SMoPT'
+def run_sim(output_path, sim_time=1500, equil_time=1000, harr_add_time=1500, cyclo_add_time=1500,
+             num_rib=200000, num_trna=3300000, num_genes=4839, cyclo_prob=0, cyclo_dissoc_rate=0, harr_rate=0,
+             random_seed=1, input_genom_path=f'{PATH_TO_SHAH}/example/input/S.cer.flash-freeze.genom',
+             input_trna_path=f'{PATH_TO_SHAH}/example/input/S.cer.tRNA', outputs=[1], logging=False):
+    """
+    Runs the Shah lab simulation with the given parameters
+    Shah simulation parameters shown in parantheses next to variable descriptions
+    
+    :param output_path: Path to directory to save output files to
+    :param sim_time: (-Tt) Total simulation time in seconds
+    :param equil_time: (-Tb) Burn-in/threshold time. Time spent by the cell to reach equilibrium.
+    :param harr_add_time: (-Th) Time at which harringtonine is added to the cell
+    :param cyclo_add_time: (-Tc) Time at which cycloheximide is added to the cell
+    :param num_rib: (-R) Total number of ribosomes in the cell
+    :param num_trna: (-t) Total number of tRNAs in the cell
+    :param num_genes: (-N) Total number of genes
+    :param cyclo_prob: (-x1) Probability of cycloheximide binding ribosomes
+    :param cyclo_dissoc_rate: (-x2) Rate of cycloheximide dissociation from bound ribosomes
+    :param harr_rate: (-y) Harringtonine rate for free ribosomes
+    :param random_seed: (-s) Random number seed
+    
+    """
+    
+    # Create output directory
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    
+    # Create output prefix using -x1, -x2, -y values
+    output_name = f'cp{float(cyclo_prob)}_cdr{float(cyclo_dissoc_rate)}_hr{float(harr_rate)}'
+    
+    # Make subdirectory inside output directory for current values
+    if not os.path.exists(output_path + output_name + '/'):
+        os.mkdir(output_path + output_name + '/')
+    
+    # Create command
+    cmd = shlex.split(f'{PATH_TO_SHAH}/bin/SMoPT_v2 -Tt {sim_time} -Tb {equil_time} -Th {harr_add_time}\
+                        -Tc {cyclo_add_time} -R {num_rib} -t {num_trna} -N {num_genes}\
+                        -F {input_genom_path} -C {input_trna_path} -x1 {cyclo_prob}\
+                        -x2 {cyclo_dissoc_rate} -y {harr_rate} -s {random_seed}\
+                        -O {output_path}{output_name}/{output_name} '\
+                        + ' '.join([f'-p{i}' for i in outputs]))
+    
+    # Run simulation, wait for it to finish
+    proc = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.PIPE)
+    if logging:
+        print(' '.join(cmd))
+    runtime = 0
+    while proc.poll() == None:
+        time.sleep(1)
+        if logging:
+            runtime += 1
+            print('Running Simulation... Runtime: %d secs' % (runtime), end = '\r')
+    if logging:
+        print()
+        print('Done')
+    
+    # Return list of saved output files 
+    return [output_path + outfile for outfile in os.listdir(output_path + output_name + '/')]
